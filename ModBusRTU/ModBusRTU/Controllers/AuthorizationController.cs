ac1ed69f787d884af71d471c601d7848fb48c849
kiselevivan1999 <kiselevi341@gmail.com>
2022-10-15 22:25:44 +0700

    Добавил визуал на авторизацию.

---
+++ ModBusRTU/ModBusRTU/Controllers/AuthorizationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModBusRTU.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Input() 
+        {
+            return View();
+        }
+    }
+}
