d2b6f1e3ef3bf06aaf9507e647e93edf4b0ac524
kiselevivan1999 <kiselevi341@gmail.com>
2022-10-15 23:21:32 +0700

    Доработа визуализация кнопки "Войти" на странице авторизации.

--- ModBusRTU/ModBusRTU/Models/User.cs
+++ ModBusRTU/ModBusRTU/Models/User.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ModBusRTU.Models
 {
     public class User
     {
+        [Display(Name = "Имя:")]
         public string Name { get; set; }
+
+        [Display(Name = "Фамилия:")]
         public string Surname { get; set; }
+
+        [Display(Name = "E-mail:")]
         public string Email { get; set; }
+        
+        [Display(Name = "Пароль:")]
         public string Password { get; set; }
     }
 }
